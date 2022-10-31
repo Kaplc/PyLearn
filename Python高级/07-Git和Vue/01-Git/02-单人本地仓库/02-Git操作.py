86ebd9c111dc0ffa4440beff94bed74fb961d5be
Kapl <755536175@qq.com>
2022-10-31 20:57:00 +0800

    2022年10月31日20:56:56

--- Python高级/07-Git和Vue/01-Git/02-单人本地仓库/02-Git操作.py
+++ Python高级/07-Git和Vue/01-Git/02-单人本地仓库/02-Git操作.py
@@ -20,9 +20,24 @@
 4- 一键提交到仓库
     git commit -am "版本描述"
 
+5- 查看历史版本
+    git log
 
+    git reflog
 
+6- 回退版本
+     git reset --hard 版本号
 
+7- 撤销修改
+    只能撤销工作区、暂存区的代码,不能撤销仓库区的代码
+    撤销仓库区的代码就相当于回退版本操作
+
+    撤销工作区:
+        git checkout 文件名
+
+    撤销暂存区:
+        1) git reset HEAD 文件名
+        2) git checkout 文件名
 
 
 """
