16707dc65f4a1c12a2adba90241d30403d5c44f9
Kapl <755536175@qq.com>
2022-10-31 20:22:33 +0800

    '2022年10月31日20:22:28'

---
+++ Python高级/07-Git和Vue/01-Git/02-单人本地仓库/01-配置本地Git.py
@@ -0,0 +1,16 @@
+"""
+1- 安装Git
+
+2- 在项目里面创建Git
+    git init
+
+3- 配置个人信息
+    git config user.name '张三'
+    git config user.email 'zhangsan@163.com'
+
+    默认不配置的话，会使用全局配置里面的用户名和邮箱
+    全局git配置文件路径：~/.gitconfig
+
+
+
+"""
