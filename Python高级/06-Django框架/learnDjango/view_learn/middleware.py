19a55b270eed4ca6f3dbd2379ef9a263126e6537
Kapl <755536175@qq.com>
2022-11-01 23:41:12 +0800

    2022年11月1日23:40:49

--- Python高级/06-Django框架/learnDjango/view_learn/middleware.py
+++ Python高级/06-Django框架/learnDjango/view_learn/middleware.py
@@ -3,7 +3,7 @@
 # 返回的中间件也是一个可以被调用的对象，并且像视图一样需要接收一个request对象参数，
 # 返回一个response对象
 
-def my_middleware(self, get_request):
+def my_middleware(get_request):
     """123"""
     # 初始化, 第一次使用中间件的时候被调用
     print("中间件初始化语句执行, 仅在服务器启动时初始化, 只执行一次")
