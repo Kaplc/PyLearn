1d1bef4e378c3d34c4b384ffcb6ce5659765edc4
Kapl <755536175@qq.com>
2022-11-23 11:37:32 +0800

    django的cache使用

---
+++ Python高级/08-美哆商城/05-用户中心/01-celery使用经验.py
@@ -0,0 +1,5 @@
+"""
+1- celery接收到任务不执行, 以下命令启动
+    celery -A celery_tasks.main worker --pool=solo -l info
+
+"""
---
+++ Python高级/08-美哆商城/05-用户中心/03-django的缓存cache使用.py
@@ -0,0 +1,8 @@
+"""
+1- 储存进去任何数据可以原封不动取出
+
+2- from django.core.cache import cache
+    存储缓存数据：cache.set('key', 内容, 有效期)
+    读取缓存数据：cache.get('key')
+    删除缓存数据：cache.delete('key')
+"""
